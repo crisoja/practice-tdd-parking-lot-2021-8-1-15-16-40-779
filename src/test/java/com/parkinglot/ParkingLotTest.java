2aebd0db758978aa3201bd9e19ebe2aa2d21d3ae
Janley Crsiologo <crisoja@oocl.com>
2021-08-07 10:39:02 +0800

    [Story 3][Janley] test: should_return_right_car_when_parking_boy_fetch_the_car_twice_given_parking_lot_with_two_parked_cars_and_two_parking_ticket()

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -106,9 +106,6 @@ public class ParkingLotTest {
         //then
         assertNotNull(parkingTicket);
     }
-//    Given a parking lot with a parked car and a parking ticket
-//    When parking boy fetch the car
-//    Then return the parked car
 
     @Test
     void should_return_the_the_actual_car_when_parking_boy_fetch_the_car_given_a_parking_lot_with_a_parked_car_and_a_parking_ticket() throws Exception {
@@ -124,4 +121,26 @@ public class ParkingLotTest {
         //then
         assertEquals(car, actualCar);
     }
+
+    @Test
+    void should_return_right_car_when_parking_boy_fetch_the_car_twice_given_parking_lot_with_two_parked_cars_and_two_parking_ticket() throws Exception {
+
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car montemayorCar = new Car();
+        Car alisonCar = new Car();
+
+        ParkingTicket montemayorParkingTicket = parkingLot.park(montemayorCar);
+        ParkingTicket alisonParkingTicket = parkingLot.park(alisonCar);
+
+        //when
+        Car actualMontemayorCar = parkingBoy.fetch(montemayorParkingTicket);
+        Car actualAlisonCar = parkingBoy.fetch(alisonParkingTicket);
+
+        //then
+        assertEquals(montemayorCar, actualMontemayorCar);
+        assertEquals(alisonCar, actualAlisonCar);
+    }
+
 }
