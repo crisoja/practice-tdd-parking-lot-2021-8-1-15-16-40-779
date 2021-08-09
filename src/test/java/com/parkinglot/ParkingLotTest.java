a05b791323e5a8b84fbec750b81827f77dd252f9
Janley Crsiologo <crisoja@oocl.com>
2021-08-09 20:30:45 +0800

    [ParkingLot][Janley] refactor: seperate parking boy test cases

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -102,55 +102,6 @@ public class ParkingLotTest {
     }
 
     @Test
-    void should_return_a_parking_ticket_when_parking_boy_park_the_car_given_a_parking_lot_and_a_car() {
-        ParkingLot parkingLot = new ParkingLot();
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
-        Car car = new Car();
-
-        //when
-        ParkingTicket parkingTicket = parkingBoy.park(car);
-
-        //then
-        assertNotNull(parkingTicket);
-    }
-
-    @Test
-    void should_return_the_the_actual_car_when_parking_boy_fetch_the_car_given_a_parking_lot_with_a_parked_car_and_a_parking_ticket() throws Exception {
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-        ParkingBoy parkingboy = new ParkingBoy(parkingLot);
-        Car car = new Car();
-        ParkingTicket parkingTicket = parkingLot.park(car);
-
-        //when
-        Car actualCar = parkingboy.fetch(parkingTicket);
-
-        //then
-        assertEquals(car, actualCar);
-    }
-
-    @Test
-    void should_return_right_car_when_parking_boy_fetch_the_car_twice_given_parking_lot_with_two_parked_cars_and_two_parking_ticket() throws Exception {
-
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
-        Car montemayorCar = new Car();
-        Car alisonCar = new Car();
-
-        ParkingTicket montemayorParkingTicket = parkingLot.park(montemayorCar);
-        ParkingTicket alisonParkingTicket = parkingLot.park(alisonCar);
-
-        //when
-        Car actualMontemayorCar = parkingBoy.fetch(montemayorParkingTicket);
-        Car actualAlisonCar = parkingBoy.fetch(alisonParkingTicket);
-
-        //then
-        assertEquals(montemayorCar, actualMontemayorCar);
-        assertEquals(alisonCar, actualAlisonCar);
-    }
-
-    @Test
     void should_throw_error_message_Unrecognized_parking_ticket_when_parking_boy_fetch_the_car_given_a_wrong_parking_ticket() throws Exception {
 
         //given
