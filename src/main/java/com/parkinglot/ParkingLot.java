fb80253843ffbab80a756bb6499fd6bc17f57fbb
Janley Crsiologo <crisoja@oocl.com>
2021-08-09 21:33:54 +0800

    [ParkingLot][Janley] refactor: format ParkingLot

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -16,61 +16,63 @@ public class ParkingLot {
 
     private int capacity;
 
-    public ParkingLot(){this(DEFAULT_CAPACITY);}
+    public ParkingLot() {
+        this(DEFAULT_CAPACITY);
+    }
 
-    public ParkingLot(int capacity){
+    public ParkingLot(int capacity) {
         parkedPosition = new HashMap<>();
         this.capacity = capacity;
     }
 
-    public ParkingTicket park(Car car){
+    public ParkingTicket park(Car car) {
 
-            if(capacity==parkedPosition.size()){
-                throw new NoAvailablePositionException();
-            }
+        if (capacity == parkedPosition.size()) {
+            throw new NoAvailablePositionException();
+        }
 
-            this.car = car;
-            ParkingTicket parkingTicket = new ParkingTicket();
-            parkedPosition.put(parkingTicket, car);
-            this.parkingTicket = parkingTicket;
+        this.car = car;
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkedPosition.put(parkingTicket, car);
+        this.parkingTicket = parkingTicket;
 
         return this.parkingTicket;
     }
 
-    public Car fetch(ParkingTicket parkingTicket){
+    public Car fetch(ParkingTicket parkingTicket) {
 
-            if(!parkedPosition.containsKey(parkingTicket)){
-               throw new UnrecognizedParkingTicketException();
-            }
-            parkedCar =  parkedPosition.get(parkingTicket);
-            parkedPosition.remove(parkingTicket, car);
-            return parkedCar;
+        if (!parkedPosition.containsKey(parkingTicket)) {
+            throw new UnrecognizedParkingTicketException();
+        }
+        parkedCar = parkedPosition.get(parkingTicket);
+        parkedPosition.remove(parkingTicket, car);
+        return parkedCar;
     }
 
-    public Car findCarFromParkingLot(){
-        return   parkedPosition
-                    .entrySet()
-                    .stream()
-                    .filter(parkingTicket -> this.parkingTicket.equals(parkingTicket.getKey()))
-                    .map(parkingTicket -> parkingTicket.getValue())
-                    .findFirst()
-                    .orElse(null);
+    public Car findCarFromParkingLot() {
+        return parkedPosition
+                .entrySet()
+                .stream()
+                .filter(parkingTicket -> this.parkingTicket.equals(parkingTicket.getKey()))
+                .map(parkingTicket -> parkingTicket.getValue())
+                .findFirst()
+                .orElse(null);
     }
 
-    public boolean isParkingLotFull(){
-        return capacity==parkedPosition.size();
+    public boolean isParkingLotFull() {
+        return capacity == parkedPosition.size();
     }
 
     public boolean checkTicket(ParkingTicket parkingTicket) {
-      return  parkedPosition.containsKey(parkingTicket);
+        return parkedPosition.containsKey(parkingTicket);
     }
 
-    public int getParkedPositionSize(){
+    public int getParkedPositionSize() {
         return parkedPosition.size();
     }
 
     public boolean findCaratParkingLot(Car car) {
-       return parkedPosition.containsValue(car);
+        return parkedPosition.containsValue(car);
 
     }
 }
