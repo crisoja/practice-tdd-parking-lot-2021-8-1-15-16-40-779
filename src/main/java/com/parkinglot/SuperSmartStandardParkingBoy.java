a345078dbbc2d2c6e68f5dfb8365106034642b63
Janley Crsiologo <crisoja@oocl.com>
2021-08-09 21:33:02 +0800

    [ParkingLot][Janley] refactor: format SuperSmartStandardParkingBoy

--- src/main/java/com/parkinglot/SuperSmartStandardParkingBoy.java
+++ src/main/java/com/parkinglot/SuperSmartStandardParkingBoy.java
@@ -4,23 +4,24 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SuperSmartStandardParkingBoy extends StandardParkingBoy {
-    public SuperSmartStandardParkingBoy(ParkingLot parkingLot){
+    public SuperSmartStandardParkingBoy(ParkingLot parkingLot) {
         super(parkingLot);
     }
-    public SuperSmartStandardParkingBoy(List<ParkingLot> parkingLotList){
+
+    public SuperSmartStandardParkingBoy(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
     }
 
     @Override
-    public ParkingTicket park(Car car){
+    public ParkingTicket park(Car car) {
 
-     return   parkingLotList.stream()
-                .max(Comparator.comparing(parkingLot ->getAvailablePositionRate(parkingLot)))
+        return parkingLotList.stream()
+                .max(Comparator.comparing(parkingLot -> getAvailablePositionRate(parkingLot)))
                 .map(parkingLot -> parkingLot.park(car))
                 .orElse(null);
     }
 
-    public double getAvailablePositionRate(ParkingLot parkingLot){
+    public double getAvailablePositionRate(ParkingLot parkingLot) {
         return (parkingLot.getCapacity() - parkingLot.getParkedPositionSize()) / parkingLot.getCapacity();
 
     }
