fd8b6cf1f8724803b374093c8a0a35dd4f4ce17d
Janley Crsiologo <crisoja@oocl.com>
2021-08-10 21:34:00 +0800

    [ParkingLot][Janley] - refactor: format NoAvailablePositionException class

--- src/main/java/com/parkinglot/NoAvailablePositionException.java
+++ src/main/java/com/parkinglot/NoAvailablePositionException.java
@@ -1,11 +1,11 @@
 package com.parkinglot;
 
-public class NoAvailablePositionException extends RuntimeException{
+public class NoAvailablePositionException extends RuntimeException {
 
     public static final String NO_AVAILABLE_POSITION = "No available position";
 
     @Override
-    public String getMessage(){
+    public String getMessage() {
         return NO_AVAILABLE_POSITION;
     }
 }
