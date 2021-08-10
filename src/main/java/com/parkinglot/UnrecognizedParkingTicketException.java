5f24155b2952ec9c88c7093c54a568c1ff90e47e
Janley Crsiologo <crisoja@oocl.com>
2021-08-10 21:59:37 +0800

    [ParkingLot][Janley] - refactor: introduce UNRECOGNIZED_PARKING_TICKET to constant and final variable

--- src/main/java/com/parkinglot/UnrecognizedParkingTicketException.java
+++ src/main/java/com/parkinglot/UnrecognizedParkingTicketException.java
@@ -2,8 +2,10 @@ package com.parkinglot;
 
 public class UnrecognizedParkingTicketException extends RuntimeException{
 
+    public static final String UNRECOGNIZED_PARKING_TICKET = "Unrecognized parking ticket";
+
     @Override
     public String getMessage(){
-        return "Unrecognized parking ticket";
+        return UNRECOGNIZED_PARKING_TICKET;
     }
 }
