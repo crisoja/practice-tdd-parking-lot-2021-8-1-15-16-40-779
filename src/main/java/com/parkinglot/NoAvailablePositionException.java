0f3d1ff40931a66bb648230bd5b26429ca13f283
Janley Crsiologo <crisoja@oocl.com>
2021-08-10 21:33:31 +0800

    [ParkingLot][Janley] - refactor: introduce No available position to constant and final

--- src/main/java/com/parkinglot/NoAvailablePositionException.java
+++ src/main/java/com/parkinglot/NoAvailablePositionException.java
@@ -1,8 +1,11 @@
 package com.parkinglot;
 
 public class NoAvailablePositionException extends RuntimeException{
+
+    public static final String NO_AVAILABLE_POSITION = "No available position";
+
     @Override
     public String getMessage(){
-        return "No available position";
+        return NO_AVAILABLE_POSITION;
     }
 }
